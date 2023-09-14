4c4606485e40b13d4f63f9f5e7a91744b0808a89
Bruno Prestes <brunoasprestes@gmail.com>
2023-09-14 10:28:52 -0300

    metodo post e put criado

--- src/controllers/livroController.js
+++ src/controllers/livroController.js
@@ -10,6 +10,19 @@ class LivroController {
     
         } catch (error) {
             console.log(error)
+            res.status(500).json({ message: `${error.message} - falha ao buscar livros`})
+        }
+    }
+
+    static async listarLivro (req, res) {
+        try {
+
+            const { id } = req.params
+            const result = await livro.findById(id)
+            res.status(200).json(result)
+    
+        } catch (error) {
+            console.log(error)
             res.status(500).json({ message: `${error.message} - falha ao buscar livro`})
         }
     }
@@ -21,6 +34,30 @@ class LivroController {
             
         } catch (error) {
             res.status(500).json({ message: `${error.message} - falha ao cadastrar livro`})
+        }
+    }
+
+    static async atualizarLivro (req, res) {
+        try {
+
+            const { id } = req.params;
+            await livro.findByIdAndUpdate(id, req.body)
+            res.status(200).json({ msg: "Livro atualizado"})
+            
+        } catch (error) {
+            res.status(500).json({ message: `${error.message} - falha ao atualizar livro`})
+        }
+    }
+
+    static async deletaLivro (req, res) {
+        try {
+
+            const { id } = req.params;
+            await livro.findByIdAndDelete(id)
+            res.status(200).json({ msg: "Livro deletado"})
+            
+        } catch (error) {
+            res.status(500).json({ message: `${error.message} - falha ao deletar livro`})
         }
     }
 }
--- src/routes/livrosRoutes.js
+++ src/routes/livrosRoutes.js
@@ -4,5 +4,9 @@ import LivroController from "../controllers/livroController.js"
 const routes = express.Router()
 
 routes.get("/livros", LivroController.listaLivros)
+routes.get("/livro/:id", LivroController.listarLivro)
+routes.post("/livros", LivroController.cadastraLivro)
+routes.put("/livro/:id", LivroController.atualizarLivro)
+routes.delete("/livro/:id", LivroController.deletaLivro)
 
 export default routes
