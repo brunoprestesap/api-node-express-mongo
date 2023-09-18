f62586d199bb0ed0bb73627af38506c2f2b180a1
Bruno Prestes <brunoasprestes@gmail.com>
2023-09-18 16:05:49 -0300

    aula 3.3

--- src/models/Livro.js
+++ src/models/Livro.js
@@ -4,7 +4,7 @@ const livroSchema = mongoose.Schema({
   titulo: { type: String, required: [true, "O título do livro é obrigatório"] },
   editora: { type: String, required: [true, "O nome da Editora é obrigatório"] },
   preco: { type: Number },
-  paginas: { type: Number },
+  paginas: { type: Number, min: [10, "O nro de paginas deve estar entre 10 e 5.000"], max: [5000, "O nro de paginas deve estar entre 10 e 5.000"] },
   autor: {
     type: mongoose.Schema.Types.ObjectId,
     ref: "autor",
